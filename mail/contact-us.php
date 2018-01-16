f1353f834b1e795467476eff71de69967d331170
Buddhika Prasanna <buddhikapbandara@gmail.com>
2018-01-16 15:52:07 +0530

    implemented get quote function

--- mail/contact-us.php
+++ mail/contact-us.php
@@ -6,15 +6,24 @@
     $to = 'buddhikapbandara@gmail.com';
     $subject = 'contact form';
 
+if(empty($_POST['contact-name'])  	||
+    empty($_POST['contact-email']) 	||
+    empty($_POST['contact-message'])	||
+    !filter_var($_POST['contact-email'],FILTER_VALIDATE_EMAIL))
+{
+    echo '<script type="text/javascript">alert("No arguments Provided!!");</script>';
+    return false;
+} else {
+
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-    echo '<script type="text/javascript">alert("hello!");</script>';
+
 
     if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) {
-            echo '<p>Your message has been sent!</p>';
+        if (mail($to, $subject, $body, $from)) {
+            echo '<script type="text/javascript">alert("Your message has been sent!");</script>';
         } else {
-            echo '<p>Something went wrong, go back and try again!</p>';
+            echo '<script type="text/javascript">alert("Something went wrong. Please try again!");</script>';
         }
     }
-
+}
 ?>
---
+++ mail/get-quote.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: buddhikaprasanna
+ * Date: 16/1/18
+ * Time: 3:18 PM
+ */
+
+// check if fields passed are empty
+if(empty($_POST['guote-name'])  	||
+    empty($_POST['quote-email']) 	||
+    empty($_POST['quote-phone'])	||
+    empty($_POST['inputService'])	||
+    empty($_POST['inputBudget'])	||
+    empty($_POST['inputTime'])	    ||
+    empty($_POST['explanation'])	||
+    !filter_var($_POST['quote-email'],FILTER_VALIDATE_EMAIL)){
+    echo '<script type="text/javascript">alert("No arguments Provided!!");</script>';
+    return false;
+} else {
+
+    $name = $_POST['guote-name'];
+    $email = $_POST['quote-email'];
+    $phone = $_POST['quote-phone'];
+    $service = $_POST['inputService'];
+    $budget = $_POST['inputBudget'];
+    $time = $_POST['inputTime'];
+    $explanation = $_POST['explanation'];
+
+    $body = "From: $name\n E-Mail: $email\n Phone: $phone\n Service: $service\n Budget: $budget\n Time: $time\n Explanation:\n $explanation";
+
+
+// create email body and send it
+    $to = 'incy@incyworld.com'; // ****PUT YOUR EMAIL ADDRESS HERE****
+    $email_subject = "Website Contact Form:  $name";
+    $email_body = "You have received a new quote request from your website contact form.\n\n" . "From: $name\n E-Mail: $email\n Phone: $phone\n Service: $service\n Budget: $budget\n Time: $time\n Explanation:\n $explanation";;
+    $headers = "From: GET-QUOTE-FORM@YOUR-WEBSITE.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
+    $headers .= "Reply-To: $email";
+    mail($to, $email_subject, $email_body, $headers);
+    echo '<script type="text/javascript">alert("Message sent successfully!");</script>';
+    return true;
+}
+?>
